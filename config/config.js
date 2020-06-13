13267d273176728ea78ce30c5243e7c7c7cff236
Tsvetan Raykov <tsvetan.raykov@gmail.com>
2020-06-13 18:54:02 +0300

    fix/env PORT

--- config/config.js
+++ config/config.js
@@ -4,6 +4,7 @@ module.exports = {
     databaseUrl: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-teomh.azure.mongodb.net/cubicle?retryWrites=true&w=majority`
   },
   production: {
+    port: process.env.PORT || 80,
     databaseUrl: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-teomh.azure.mongodb.net/cubicle?retryWrites=true&w=majority`
   }
 }
